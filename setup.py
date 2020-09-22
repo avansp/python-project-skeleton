d36d9a0090fd682c8251392155a391b4dfa8d985
Avan Suinesiaputra <avan.sp@gmail.com>
2020-09-22 15:39:34 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pconv-keras',
+    author='Mathias Gruber',
+    author_email='somehwere@world.com',
+    version='0.1.0',
+    license='LICENSE',
+    description='pconv',
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+)
